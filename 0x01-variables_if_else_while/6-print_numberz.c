46de9b0887027097295eda174d352acd32bb8c35
“Solomon <solomon.oduniyi@gmail.com>
2022-09-09 12:54:30 -0700

    Task completed

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+nclude <stdio.h>
+/**
+ *  * main - print if the number is postive, zero, or negative
+ *   *
+ *    * Description: using the main function
+ *     * this program prints "Programming is positive, zero, or negative
+ *      * Return: 0
+ *       */
+int main(void)
+{
+int i
+for (i = 48; i <=57; i++);
+{
+putchar(i);
+}
+putchar('\n');
+return (0);
+}
+
