e5db336afd4fc71bcdb75bc84b20c4a3dbe7c49b
“Solomon <solomon.oduniyi@gmail.com>
2022-09-09 15:19:12 -0700

    Task

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.o>
+/**
+ * Return: 0 if success
+ */
+
+int main(void)
+{
+	put("\"Programming is like building a multilingual puzzle");
+	putchar('\n');
+	return(0);
+}
+
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.o>
+/**
+ * Return: 0 if success
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return(0);
+}
+
