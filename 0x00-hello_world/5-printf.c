192bbafb524c8f3ba69570ce7598dc5a5ad9626d
“Solomon <solomon.oduniyi@gmail.com>
2022-09-17 08:32:32 -0700

    5-printf.cUpdated

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,10 @@
-#include <stdio.o>
-/**
- * Return: 0 if success
- */
-
-int main(void)
-{
-	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return(0);
-}
-
+#include <stdio.o>                                                                                                                                                                  
+/**                                                                                                                                                                                 
+ * Return: 0 if success                                                                                                                                                             
+ */                                                                                                                                                                                 
+                                                                                                                                                                                    
+int main(void)                                                                                                                                                                      
+{                                                                                                                                                                                   
+        printf("with proper grammar, but the outcome is a piece of art,\n");                                                                                                        
+        return(0);                                                                                                                                                                  
+}  
