d218f629c125284867b9965acab0214a440fed5b
Solomon Oduniyi <solomon.oduniyi@gmail.com>
2022-12-10 08:08:15 -0800

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,4 +1,5 @@
-#include <stdio.o>
+#include <stdio.h>
+
 /**
  * Return: o if success
  */
@@ -10,6 +11,6 @@ int main(void)
 	printf("Size of a long int: %d byte(s)\n", sizeof(long int));
 	printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
 	printf("Size of a float: %d byte(s)\n", sizeof(float));
-	return(0);
+	return (0);
 }
 
