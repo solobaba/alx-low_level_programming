975d4bf6099373f5bda375b2ad97d51d5da2081d
Solomon Oduniyi <solomon.oduniyi@gmail.com>
2022-12-11 11:13:54 -0800

    8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,23 +1,27 @@
 #include <stdio.h>
 /**
- *  * main - print if the number is postive, zero, or negative
- *   *
- *    * Description: using the main function
- *     * this program prints "Programming is positive, zero, or negative
- *      * Return: 0
- *       */
+ * main - print if the number is postive, zero, or negative
+ *
+ * Description: using the main function
+ * this program prints programming is positive, zero, or negative
+ * Return: 0
+ */
 int main(void)
 {
-char c;
-for (c = '0'; c <= '9'; c++)
-{
-putchar(c);
-}
-for (c = 'a'; c <= 'f'; c++)
-{
-putchar(c);
-}
-putchar('\n');
-return (0);
-}
+	char c = 'a';
+	int num;
 
+	for (num = 0; num < 16; num++)
+	{
+		if (num < 10)
+		{
+			putchar(num + '0');
+		} else
+		{
+			putchar(c);
+			c++;
+		}
+	}
+	putchar('\n');
+	return (0);
+}
