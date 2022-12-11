ce613e32cf4207b06f60d6cfe8f9869967512379
Solomon Oduniyi <solomon.oduniyi@gmail.com>
2022-12-11 10:55:48 -0800

    5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -8,11 +8,9 @@
  */
 int main(void)
 {
-int i
-for (i = 0; i < 10; i++)
-{
-	printf("%i", i);
-}
-	printf("\n")
-	return (0)
+	int i;
+	for (i = 0; i < 10; i++)
+		printf("%d", i);
+	putchar("\n");
+	return (0);
 }
