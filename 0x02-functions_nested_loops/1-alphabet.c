c93af19b9c39493248e3f3f0749a2d2a91ed2fc5
Solomon Oduniyi <solomon.oduniyi@gmail.com>
2022-12-13 13:01:57 -0800

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
- *  * print_alphabet - function to print abc
- *   *
- *    * Return: 0
- *     */
+ * print_alphabet - function to print abc
+ *
+ * Return: 0
+ */
 void print_alphabet(void)
 {
 	char c;
