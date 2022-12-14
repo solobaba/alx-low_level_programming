0713b791eec1e3c1c4ddf3ae2d928c8838a273e3
Solomon Oduniyi <solomon.oduniyi@gmail.com>
2022-12-14 10:08:24 -0800

    2-print_alphabet_x10.c_updated

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,12 +1,15 @@
 #include "main.h"
+
 /**
  * print_alphabet_x10 - function to print abc 10 times
  *
  * Return: 0
- */
+*/
+
 void print_alphabet_x10(void)
 {
 	char c, i;
+
 	for (i = 0; i <= 9; i++)
 	{
 		for (c = 'a'; c <= 'z'; c++)
@@ -16,3 +19,4 @@ void print_alphabet_x10(void)
 		_putchar('\n');
 	}
 }
+
