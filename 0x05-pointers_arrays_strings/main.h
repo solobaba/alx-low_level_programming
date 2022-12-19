d46b0d9a3e20f6a882f6cc7126b5e248efe6e7ed
Solomon Oduniyi <solomon.oduniyi@gmail.com>
2022-12-19 12:34:34 -0800

    0x05-pointers_arrays_strings

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -2,9 +2,9 @@
 #define MAIN_H
 
 /**
- * * void prototypes()
- * * int prototypes()
- * */
+ * void prototypes()
+ * int prototypes()
+ */
 
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
