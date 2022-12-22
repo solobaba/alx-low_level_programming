58f3178cd333aa86e5f658275796d7b25db2971c
Solomon Oduniyi <solomon.oduniyi@gmail.com>
2022-12-22 12:01:53 -0800

    main.h

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -3,10 +3,10 @@
 int _putchar(char c);
 #include <stdio.h>
 /**
- *  *void prototypes()
- *   *int prototypes()
- *    *char prototypes()
- *     */
+ * void prototypes()
+ * int prototypes()
+ * char prototypes()
+ */
 void reverse_array(int *a, int n);
 void print_number(int n);
 void print_buffer(char *b, int size);
