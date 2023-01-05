bfc7e8147e1b4dda723ce63feb6e1428054c6b9d
Solomon Oduniyi <solomon.oduniyi@gmail.com>
2023-01-05 08:44:46 -0800

    2-strlen_recursion.c

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - eturns the length of a string
+ * @s: pointer to string
+ *
+ * Retrun: length of string
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (*s)
+	{
+		return (1 + _strlen_recursion(s + 1));
+	}
+	else
+	{
+		return (0);
+	}
+}
--- 0x08-recursion/README.md
+++ 0x08-recursion/README.md
@@ -1,1 +1,1 @@
-1-print_rev_recursion.c
+2-strlen_recursion.c
