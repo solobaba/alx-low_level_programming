4551ebfd57ace43901ca2919b4148b84814e784e
Solomon Oduniyi <solomon.oduniyi@gmail.com>
2023-01-16 11:21:39 -0800

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,7 +1,7 @@
-#ifndef STRTYP                                                                                                                                                  
-#define STRTYP                                                                                                                                                  
-                                                                                                                                                                
-/**                                                                                                                                                             
+#ifndef STRTYP
+#define STRTYP
+
+/**
  * struct dog - structure of a dog.
  * @name: name of  dog.
  * @age: age of dog.
@@ -15,6 +15,7 @@ struct dog
 };
 
 typedef struct dog dog_t;
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char * owner);
