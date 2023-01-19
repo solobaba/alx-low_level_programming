aeca40843c026e0ecfc4b7ee62dca00ab409a2c4
Solomon Oduniyi <solomon.oduniyi@gmail.com>
2023-01-19 13:03:22 -0800

    1-print_numbers.c

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -20,7 +20,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	for (i = 0; i < n; i++)
 	{
 		printf("%d", va_arg(numners, int));
-		if (i < n - 1 && separator != null)
+		if (i < n - 1 && separator != NULL)
 			printf("%s", separator);
 	}
 	printf("\n");
